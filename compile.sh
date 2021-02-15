95b3cc4d7897b79c988c63289a32d7606133eb79
eginatakasan <13517030@std.stei.itb.ac.id>
2021-02-15 21:56:29 +0700

    Add read and write for PPM images

---
+++ compile.sh
@@ -0,0 +1,1 @@
+g++ main.cpp src/imageprocessing.cpp -o main
--- src/image.h
+++ src/image.h
@@ -2,15 +2,24 @@
 #define IMAGE_H
 
 #include <string>
+using std::string;
 
 class Image {
     private:
         /* Variables */
+        int** content;
+        char* magicNumber; //buat nentuin format gambar 
+        int maxVal; //maximum color value for a pixel
         char* path;
-        int width; 
-        int height;
-        int bitperpixel;
-        long filesize;
+
+        int width; //lebar gambar
+        int height; //tinggi gambar
+        int pixels; //jumlah total pixels
+        int bitperpixel; //jumlah bits untuk setiap pixel
+        long filesize; //ukuran file gambar
+
+        /* Functions */
+        bool isFormatValid(char* magicNumber);
 
     public:
         /* Constructor */
@@ -18,6 +27,7 @@ class Image {
 
         /* Functions */
         void write(char* const path);
+        void showDetails();
 
         /* Getter Functions */
         char* getPath();
